9d5354309b22abe6b49eaa23a4f29a5a05497180
dukey1221 <rubenestor816@gmail.com>
2021-09-24 12:45:41 +0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,95 @@
+@extends('base')
+
+@section('content')
+
+<style></style>
+
+  <div class="container">
+  <h1 class="title text-danger" >Welcome To AnimeTV  </h1>
+  </div>
+
+  <style>
+
+    .center {
+      padding: 70px 0;
+      border: 3px solid green;
+      text-align: center;
+      margin-top: 15%;
+    }
+    </style>
+    <br>
+    <br>
+    <div class="container">
+    
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">One Piece</h5>
+                <p class="card-text">One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since July 1997, with its individual chapters compiled into 100 tankōbon volumes as of September 2021.</p>
+                <a href="#" class="btn btn-danger">Watch for Free</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="2.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Naruto</h5>
+                <p class="card-text">Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.</p>
+                <a href="#" class="btn btn-danger">Watch for Free</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="3.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Demon Slayer</h5>
+                <p class="card-text">Demon Slayer: Kimetsu no Yaiba is a Japanese manga series written and illustrated by Koyoharu Gotouge. It follows teenage Tanjiro Kamado, who strives to become a demon slayer after his family is slaughtered and his younger sister Nezuko is turned into a demon.</p>
+                <a href="#" class="btn btn-danger">Watch for Free</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img src="img4.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Dr. Stone</h5>
+              <p class="card-text">Dr. Stone is a Japanese manga series written by Riichiro Inagaki and illustrated by the South Korean artist Boichi. It has been serialized in Shueisha's Weekly Shōnen Jump since March 2017, with its chapters collected in twenty-two tankōbon volumes as of August 2021.</p>
+              <a href="#" class="btn btn-danger">Watch for Free</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img src="img5.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Attack on Titan</h5>
+              <p class="card-text">Attack on Titan is a Japanese manga series written and illustrated by Hajime Isayama. It is set in a world where humanity lives inside cities surrounded by three enormous walls that protect them</p>
+              <a href="#" class="btn btn-danger">Watch for Free</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img src="img6.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Dragon Ball Z</h5>
+              <p class="card-text">"Dragon Ball Z" follows the adventures of Goku who, along with the Z Warriors, defends the Earth against evil. The action adventures are entertaining and reinforce the concept of good versus evil. "Dragon Ball Z" teaches valuable character virtues such as teamwork, loyalty, and trustworthiness.</p>
+              <a href="#" class="btn btn-danger">Watch for Free</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+@endsection
+
